7339b7366f39f55d0003cc8e3db791b6ce721644
nakodila <elenatalk@gmail.com>
2018-01-21 18:32:47 -0500

    added cat and mouse

--- src/components.js
+++ src/components.js
@@ -1,1 +1,81 @@
+const component = function component (width, height, path, sx, sy, dx, dy, dw, dh,  type, comp) {
+    this.type = type;
+    if (type == "image") {
+        this.image = new Image();
+        this.image.src = path;
+    }
 
+    this.width = width;
+    this.height = height;
+    this.speedX = 0;
+    this.speedY = 0;
+    this.sx = sx;
+    this.sy = sy;
+    this.dx = dx;
+    this.dy = dy;
+    this.dw = dw;
+    this.dh = dh;
+
+    this.catFrames = { 0 : [1, 1],
+                        1 : [150, 1],
+                        2 : [300, 1],
+                        3 : [450, 1],
+                        4 : [1, 117],
+                        5 : [150, 117],
+                        6 : [300, 117],
+                        7 : [450, 117],
+                        8 : [1, 236],
+                        9 : [150, 236],
+                        10 : [300, 236],
+                        11 : [450, 236]
+                      };
+
+    this.frameIndex = 0;
+    this.pressCount = 0;
+    this.pressPerFrame = 20;
+
+    this.update = function() {
+        ctx = myGameArea.context;
+        if (this.pressCount > this.pressPerFrame) {
+          this.pressCount = 0;
+          this.frameIndex += 1;
+          if (this.frameIndex == 5) {
+            this.frameIndex = 0;
+          }
+        }
+        if ((type == "image") && (comp == "cat")) {
+            ctx.drawImage(
+                this.image,
+                this.catFrames[Math.floor(this.frameIndex)][0],
+                this.catFrames[Math.floor(this.frameIndex)][1],
+                this.width,
+                this.height,
+                this.dx,
+                this.dy,
+                this.dw,
+                this.dh
+              );
+        } else if (type == "image") {
+            ctx.drawImage(
+                this.image,
+                this.sx,
+                this.sy,
+                this.width,
+                this.height,
+                this.dx,
+                this.dy,
+                this.dw,
+                this.dh
+              );
+        } else {
+            ctx.fillStyle = path;
+            ctx.fillRect(this.dx, this.dy, this.dw, this.dh);
+        }
+    }
+    this.newPos = function() {
+        this.dx += this.speedX;
+        this.dy += this.speedY;
+    }
+}
+
+module.exports = component;
